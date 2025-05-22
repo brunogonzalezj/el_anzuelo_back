f55448f5af52b4d073e4ccab720875d75702ef41
Brunogonzalez00 <a2021113701@estudiantes.upsa.edu.bo>
2025-05-22 11:23:38 -0400

    Perla

--- src/features/platos/plato.route.ts
+++ src/features/platos/plato.route.ts
@@ -10,15 +10,15 @@ export const platoRouter = Router();
 //platoRouter.use(authMiddleware);
 
 // Rutas públicas para usuarios autenticados
-platoRouter.get('/', platoController.getPlatos);
-platoRouter.get('/:id', platoController.getPlatoById);
+platoRouter.get('/', authMiddleware, platoController.getPlatos);
+platoRouter.get('/:id', authMiddleware,platoController.getPlatoById);
 
 // Rutas para administración de platos (solo roles autorizados)
 const adminRoles = ['ENCARGADO', 'CHEF'];
-platoRouter.post('/',  platoController.createPlato);
-platoRouter.put('/:id', roleMiddleware(adminRoles), platoController.updatePlato);
-platoRouter.delete('/:id', roleMiddleware(adminRoles), platoController.deletePlato);
+platoRouter.post('/', authMiddleware, platoController.createPlato);
+platoRouter.put('/:id',authMiddleware, roleMiddleware(adminRoles), platoController.updatePlato);
+platoRouter.delete('/:id', authMiddleware,roleMiddleware(adminRoles), platoController.deletePlato);
 
 // Rutas para gestionar extras en platos
-platoRouter.post('/:id/extras', roleMiddleware(adminRoles), platoController.asignarExtras);
-platoRouter.delete('/:id/extras/:extraId', roleMiddleware(adminRoles), platoController.quitarExtra);
+platoRouter.post('/:id/extras', authMiddleware,roleMiddleware(adminRoles), platoController.asignarExtras);
+platoRouter.delete('/:id/extras/:extraId', authMiddleware,roleMiddleware(adminRoles), platoController.quitarExtra);
